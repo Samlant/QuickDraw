abeb97114cdd1de6c09f7dfff75932a60482f15b
Sam <7569798+Samlant@users.noreply.github.com>
2022-12-10 08:48:10 -0500

    Added HTML body message to child SeaWave class
    Added a specific HTML body message to the child SeaWave class.  This HTML message includes a formatted salutation, body, and signature.

--- Send_email.py
+++ Send_email.py
@@ -17,6 +17,22 @@ class Send_email:
 class Seawave(Send_email):
     send_to_seawave = Seawave
     mail.to = 'boatprograms@one80intermediaries.com'
-    mail.HTMLBody = '<h2><p>Hey BoatPrograms,</p><p>Please see the attached for a new quote submission for the Seawave market.  Thanks in advance for your consideration.</p></h2>
+    mail.HTMLBody = 
+    '<p><span style="color: #000080;">Hey BoatPrograms,</span></p>
+    <p>&nbsp;</p>
+    <p><span style="color: #000080;">Please see the attached for a new quote submission for the SeaWave market. Thanks in advance for your consideration.</span></p>
+    <p>&nbsp;</p>
+    <p><span style="color: #000080;">With Pleasure,</span></p>
+    <p><span style="color: #000080;"><strong>Samuel Alexander Lanteigne</strong></span></p>
+    <p><span style="color: #000080;">NOVAMAR INSURANCE GROUP</span></p>
+    <p><span style="color: #000080;">Main :&nbsp;(800)-823-2798</span></p>
+    <p><span style="color: #000080;">Office : (941)-444-5099</span></p>
+    <p><span style="color: #000080;">Fax : (941)-328-3598</span></p>
+    <p><span style="color: #000080;">1549 Ringling Blvd., Suite 101</span></p>
+    <p><span style="color: #000080;">Sarasota, FL 34236&nbsp;</span></p>
+    <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com/">www.novamarinsurance.com</a></span></p>
+    <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com.mx/">www.novamarinsurance.com.mx</a></span></p>
+    <p><span style="color: #000080;">Established in 1987 with offices in: Seattle | Newport Beach | San Diego | Sarasota | Jacksonville | Puerto Vallarta | Cancun | San Miguel de Allende&nbsp;</span></p>
+    <p><span style="color: #000080;">Please be advised that coverage is not bound, renewed, amended or in force unless confirmed in writing by a Novamar Insurance Group agent or by the represented company.</span></p>'
     
     
