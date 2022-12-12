ce89648c59c9a39d629045f4d19f60bad5de3c9f
Sam <7569798+Samlant@users.noreply.github.com>
2022-12-12 15:33:11 -0500

    Added comments to structure program file
    Added comments and placeholder text in order to better structure the ongoing development of this project.  Will need to complete each section and debug as necessary.

--- Send_email.py
+++ Send_email.py
@@ -1,4 +1,13 @@
-class Send_email:
+#Code to define GUI Launcher
+#above code placeholder here
+
+#Code to launch GUI Launcher
+#above code placeholder here
+
+#Code to save exported values upon clicking submit button to variables
+#above code placeholder here
+
+#Defining Sending email PARENT class,  containing the mail.Send() function callclass Send_email:
     sender_address = 'blank'
     import win32com.client as win32
     outlook = win32.Dispatch('outlook.application')
@@ -13,26 +22,32 @@ class Send_email:
     def send_it(self):
         mail.Send()
         return True
-
+                    
+#Code for child classes for each carrier
 class Seawave(Send_email):
-    send_to_seawave = Seawave
-    mail.to = 'boatprograms@one80intermediaries.com'
-    mail.HTMLBody = 
-    '<p><span style="color: #000080;">Hey BoatPrograms,</span></p>
-    <p>&nbsp;</p>
-    <p><span style="color: #000080;">Please see the attached for a new quote submission for the SeaWave market. Thanks in advance for your consideration.</span></p>
-    <p>&nbsp;</p>
-    <p><span style="color: #000080;">With Pleasure,</span></p>
-    <p><span style="color: #000080;"><strong>Samuel Alexander Lanteigne</strong></span></p>
-    <p><span style="color: #000080;">NOVAMAR INSURANCE GROUP</span></p>
-    <p><span style="color: #000080;">Main :&nbsp;(800)-823-2798</span></p>
-    <p><span style="color: #000080;">Office : (941)-444-5099</span></p>
-    <p><span style="color: #000080;">Fax : (941)-328-3598</span></p>
-    <p><span style="color: #000080;">1549 Ringling Blvd., Suite 101</span></p>
-    <p><span style="color: #000080;">Sarasota, FL 34236&nbsp;</span></p>
-    <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com/">www.novamarinsurance.com</a></span></p>
-    <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com.mx/">www.novamarinsurance.com.mx</a></span></p>
-    <p><span style="color: #000080;">Established in 1987 with offices in: Seattle | Newport Beach | San Diego | Sarasota | Jacksonville | Puerto Vallarta | Cancun | San Miguel de Allende&nbsp;</span></p>
-    <p><span style="color: #000080;">Please be advised that coverage is not bound, renewed, amended or in force unless confirmed in writing by a Novamar Insurance Group agent or by the represented company.</span></p>'
-    
+    if seawave_check = True :
+      send_to_seawave = Seawave
+      mail.to = 'boatprograms@one80intermediaries.com'
+      mail.HTMLBody = 
+      '<p><span style="color: #000080;">Hey BoatPrograms,</span></p>
+      <p>&nbsp;</p>
+      <p><span style="color: #000080;">Please see the attached for a new quote submission for the SeaWave market. Thanks in advance for your consideration.</span></p>
+      <p>&nbsp;</p>
+#Insert my below signature into the parent email HTML body,  unless there's a specific command to insert specific signature from Outlook (mail.InsertSig for example?)
+      <p><span style="color: #000080;">With Pleasure,</span></p>
+      <p><span style="color: #000080;"><strong>Samuel Alexander Lanteigne</strong></span></p>
+      <p><span style="color: #000080;">NOVAMAR INSURANCE GROUP</span></p>
+      <p><span style="color: #000080;">Main :&nbsp;(800)-823-2798</span></p>
+      <p><span style="color: #000080;">Office : (941)-444-5099</span></p>
+      <p><span style="color: #000080;">Fax : (941)-328-3598</span></p>
+      <p><span style="color: #000080;">1549 Ringling Blvd., Suite 101</span></p>
+      <p><span style="color: #000080;">Sarasota, FL 34236&nbsp;</span></p>
+      <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com/">www.novamarinsurance.com</a></span></p>
+      <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com.mx/">www.novamarinsurance.com.mx</a></span></p>
+      <p><span style="color: #000080;">Established in 1987 with offices in: Seattle | Newport Beach | San Diego | Sarasota | Jacksonville | Puerto Vallarta | Cancun | San Miguel de Allende&nbsp;</span></p>
+      <p><span style="color: #000080;">Please be advised that coverage is not bound, renewed, amended or in force unless confirmed in writing by a Novamar Insurance Group agent or by the represented company.</span></p>'
+    else :
+      pass
+#The above code to be debugged first,  then expanded on to include other carriers within separate child classes.
+                    
     
