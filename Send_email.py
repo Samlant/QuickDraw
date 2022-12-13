0e8c6735953d16df1867a7709f6ab1a688068df9
Sam <7569798+Samlant@users.noreply.github.com>
2022-12-12 23:04:07 -0500

    Created instances to send to each carrier
    Reverted from creating child classes (deleted them) to instead simply creating instances for each carrier and included all carriers that we submit to. Maybe we can try looping to condense code?

--- Send_email.py
+++ Send_email.py
@@ -7,47 +7,123 @@
 #Code to save exported values upon clicking submit button to variables
 #above code placeholder here
 
-#Defining Sending email PARENT class,  containing the mail.Send() function callclass Send_email:
-    sender_address = 'blank'
+#Defining Sending email PARENT class,  containing the mail.send() call under the function send_it(). can condense once debugged.
+class Send_email:
+    def __init__(self, fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    self.fname = fname
+    self.lname = lname
+    self.byear = byear
+    self.bmake = bmake
+    self.blength = blength
+    self.carrier = carrier
+    self.recipient = recipient
+    self.cc_address = cc_address
+    
     import win32com.client as win32
-    outlook = win32.Dispatch('outlook.application')
-    mail = outlook.CreateItem(0)
-    mail.To = 'To address'
-    mail.Subject = (last_name, + ' ' + first_name + ' | ' + boat_year + ' ' + boat_make + ' ' + boat_length + ' | ' + New Quote Submission')
-    mail.Body = 'Message body'
-    mail.HTMLBody = '<h2>Message_set_as_variable</h2>' #this field is optional
-    attachment  = "Path-to-attachment" # To attach a file to the email (optional):
-    mail.Attachments.Add(attachment)
+    
+    def create_msg(self):
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.To = self.recipient
+        if self.cc_address != "null":
+            mail.CC = self.cc_address
+        else:
+            pass
+        mail.Subject = (lname + ', ' + fname + ' | ' + byear + ' ' + bmake + ' ' + blength + ' | ' + 'New Quote Submission for ' + carrier)
+        mail.HTMLBody = html_message #body and signature in HTML
+        #attachment  = "Path-to-attachment" #need to insert from gui launcher
+        #mail.Attachments.Add(attachment) 
 
     def send_it(self):
-        mail.Send()
-        return True
+        try:
+          mail.Send()
+        except:
+            print(cannot send email)
+        else:
+          print("Email sent")
+          return True
                     
-#Code for child classes for each carrier
-class Seawave(Send_email):
-    if seawave_check = True :
-      send_to_seawave = Seawave
-      mail.to = 'boatprograms@one80intermediaries.com'
-      mail.HTMLBody = 
-      '<p><span style="color: #000080;">Hey BoatPrograms,</span></p>
-      <p>&nbsp;</p>
-      <p><span style="color: #000080;">Please see the attached for a new quote submission for the SeaWave market. Thanks in advance for your consideration.</span></p>
-      <p>&nbsp;</p>
-#Insert my below signature into the parent email HTML body,  unless there's a specific command to insert specific signature from Outlook (mail.InsertSig for example?)
-      <p><span style="color: #000080;">With Pleasure,</span></p>
-      <p><span style="color: #000080;"><strong>Samuel Alexander Lanteigne</strong></span></p>
-      <p><span style="color: #000080;">NOVAMAR INSURANCE GROUP</span></p>
-      <p><span style="color: #000080;">Main :&nbsp;(800)-823-2798</span></p>
-      <p><span style="color: #000080;">Office : (941)-444-5099</span></p>
-      <p><span style="color: #000080;">Fax : (941)-328-3598</span></p>
-      <p><span style="color: #000080;">1549 Ringling Blvd., Suite 101</span></p>
-      <p><span style="color: #000080;">Sarasota, FL 34236&nbsp;</span></p>
-      <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com/">www.novamarinsurance.com</a></span></p>
-      <p><span style="color: #000080;"><a style="color: #000080;" href="http://www.novamarinsurance.com.mx/">www.novamarinsurance.com.mx</a></span></p>
-      <p><span style="color: #000080;">Established in 1987 with offices in: Seattle | Newport Beach | San Diego | Sarasota | Jacksonville | Puerto Vallarta | Cancun | San Miguel de Allende&nbsp;</span></p>
-      <p><span style="color: #000080;">Please be advised that coverage is not bound, renewed, amended or in force unless confirmed in writing by a Novamar Insurance Group agent or by the represented company.</span></p>'
-    else :
-      pass
-#The above code to be debugged first,  then expanded on to include other carriers within separate child classes.
+#code to create instances
+if seawave_check == True:
+    recipient = 'boatprograms@one80intermediaries.com'
+    carrier = seawave
+    html_message = 'tba'
+    seawave = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    seawave.send_it()
+else:
+    pass
+
+if primetime_check == True
+    recipient = 'boatprograms@one80intermediaries.com'
+    carrier = Prime Time Insurance'
+    html_message = 'tba'
+    primetime = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    primetime.send_it()
+else:
+    pass
+    
+if americanmodern_check == True:
+    recipient = 'boatbrokerage@one80intermediaries.com'
+    carrier = 'American Modern'
+    html_message = 'tba'
+    americanmodern = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    americanmodern.send_it()
+else:
+    pass
+    
+if kemah_check == True:
+    recipient = 'tom.carroll@kemahcapital.com'
+    carrier = 'Kemah Marine'
+    html_message = 'tba'
+    kemah = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    kemah.send_it()
+else:
+    pass
+    
+if concept_check == True:
+    recipient = 'quote.team@special-risks.co.uk'
+    carrier = 'Concept'
+    html_message = 'tba'
+    concept = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    concept.send_it()
+else:
+    pass
+    
+if yachtinsure_check == True:
+    recipient = 'quotes@yachtinsure.uk.com'
+    carrier = 'Yachtinsure'
+    html_message = 'tba'
+    yachtinsure = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    yachtinsure.send_it()
+else:
+    pass
+    
+if intact_check == True:
+    recipient = 'yucusa@intactinsurance.com'
+    carrier = 'Intact Insurance'
+    html_message = 'tba'
+    intact = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    intact.send_it()
+else:
+    pass
+
+if travelers_check == True:
+    recipient = 'ytquote@travelers.com'
+    html_message = 'tba'
+    travelers = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    travelers.send_it()
+else:
+    pass
+    
+if newhampshire_check == True:
+    recipient = 'boatprograms@one80intermediaries.com'
+    carrier = 'New Hampshire'
+    html_message = 'tba'
+    newhampshire = Send_email(fname, lname, byear, bmake, blength, carrier, recipient, cc_address, html_message)
+    newhampshire.send_it()
+else:
+    pass
+    
+#EoF
                     
     
