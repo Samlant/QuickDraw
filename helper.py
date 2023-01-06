88ee88f4fdacc1c2d676b281d13ae77829308e9c
Sam L <7569798+Samlant@users.noreply.github.com>
2023-01-06 07:44:05 -0500

    Configured main script to align to updated standards
    Updated entire script to newer standards by comprehensively updating each function to reflect what I know best so far.  Few tweaks are still needed but the script is nearly complete.

--- helper.py
+++ helper.py
@@ -12,13 +12,6 @@ def update_config():
     updater = ConfigUpdater()
     updater.read("configurations.ini")
     return updater
-
-def update_def_cc(cc_1, cc_2, merge):
-    
-    updater['CarbonCopy Settings']['def_cc_address_1'].value = cc_1
-    updater['CarbonCopy Settings']['def_cc_address_2'].value = cc_2
-    updater['CarbonCopy Settings']['settings_merge_cc_addresses'].value = merge
-    print('complete')
     
 #    print(carrier)
 #    updater["CarbonCopy Settings"]["def_cc_address_1"].value = "Alan Turing"
--- submit_script.py
+++ submit_script.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from tkinter.ttk import Notebook, Style
 from tkinter import * 
 from tkinterdnd2 import *
-import fillpdf as fillpdf
+import fillpdf
 from helper import *
 
 #Global variables
@@ -17,23 +17,63 @@ length = 0
 #-----------------------
 
 #Functions--------------
-#SETTINGS - Save & update#
-def btn_save_carrier_details():
-	try:
-		carrier = dropdown_email_template.get()
-		email = .get()
-		greeting = .get()
-		body = .get()
-		salutation = .get()
-		#??? whatisthis? update_carrier_email('SW', 'boatprograms@one80intermediaries.com', 'Hey Boat Programs,', 'Please see the attached for a new quote submission for the Seawave market. Thank you in advance for your consideration of our client. ', '')
-		#copy below to 
-	insert function to update config file with these values and save
+#SETTINGS - Save & update
+def btn_save_carrier_template():
+    updater = update_config()
+    try:
+        carrier = dropdown_email_template.get()
+        if carrier!='Combo SW and PT' or 'Combo SW and NH' or 'Combo SW, PT and NH' or 'Combo SW, PT and NH':
+            if carrier=='Seawave':
+                carrier = 'SW email'
+            if carrier=='Prime Time':
+                carrier = 'PT email'
+            if carrier=='New Hampshire':
+                carrier = 'NH email'
+            if carrier=='American Modern':
+                carrier = 'AM email'
+            if carrier=='Kemah':
+                carrier = 'KM email'
+            if carrier=='Concept':
+                carrier = 'CP email'
+            if carrier=='Yachtinsure':
+                carrier = 'YI email'
+            if carrier=='Century':
+                carrier = 'CE email'
+            if carrier=='Intact':
+                carrier = 'IN email'
+            if carrier=='Travelers':
+                carrier = 'TV email'
+            updater[carrier]['address'].value = carrier_address.get()
+            updater[carrier]['greeting'].value = carrier_greeting.get()
+            updater[carrier]['body'].value = carrier_body.get()
+            updater[carrier]['salutation'].value = carrier_salutation.get()
+        else:
+            if carrier=='Combo SW and PT':
+                carrier = 'Combo email'
+                key = 'sw_and_pt_body'
+                updater[carrier][key].value = carrier_body.get()
+            if carrier=='Combo SW and NH':
+                carrier = 'Combo email'
+                key = 'sw_and_nh_body'
+                updater[carrier][key].value = carrier_body.get()
+            if carrier=='Combo SW, PT and NH':
+                carrier = 'Combo email'
+                key = 'pt_and_nh_body'
+                updater[carrier][key].value = carrier_body.get()
+            if carrier=='Combo PT and NH':
+                carrier = 'Combo email'
+                key = 'pt_and_nh_and_sw_body'
+                updater[carrier][key].value = carrier_body.get()
+        updater['General settings']['your_name'].value = your_name.get()
+    except:
+        print('save carrier template to config file FAILED!')
 
-def btnSave_Settings():  #NEED TO DOUBLE CHECK & EDIT,  and copy for other settings...
-	updater["CC-address Settings"]["settings_merge_cc_addresses"].value = settings_merge_cc_addresses.get()
-	updater["CC-address Settings"]["def_cc_address_1"].value = def_cc_address_1.get()
-	updater["CC-address Settings"]["def_cc_address_2"].value = def_cc_address_2.get()
-	
+def btnSaveMainSettings():
+    updater = update_config()
+    updater["CC-address Settings"]["settings_merge_cc_addresses"].value = settings_merge_cc_addresses.get()
+    updater["CC-address Settings"]["def_cc_address_1"].value = def_cc_address_1.get()
+    updater["CC-address Settings"]["def_cc_address_2"].value = def_cc_address_2.get()
+
 #End of SETTINGS#
 
 # Helper Functions------------------
@@ -50,33 +90,35 @@ def Get_Path(event):
 	return Get_Path.quoteform_path
 
 def Get_Subject():
-	needed_values_dict = fillpdfs.get_form_fields(Get_Path.quoteform_path)
-	needed_values_dict = {key: needed_values_dict[key] for key in needed_values_dict.keys()
+    needed_values_dict = fillpdfs.get_form_fields(Get_Path.quoteform_path)
+    needed_values_dict = {key: needed_values_dict[key] for key in needed_values_dict.keys()
        & {'4669727374204e616d65', '4c617374204e616d65', 'Year', '4d616b6520616e64204d6f64656c', 'Length'}}
-	first_name = needed_values_dict.get('4669727374204e616d65')
-	last_name = needed_values_dict.get('4c617374204e616d65')
-	year = needed_values_dict.get('Year')
-	make = needed_values_dict.get('4d616b6520616e64204d6f64656c')
-	length = needed_values_dict.get('Length')
-	msg_subject = f"{last_name}, {first_name} | {year} {make} {length} | New Quote Submission"
-	return msg_subject
+    first_name = needed_values_dict.get('4669727374204e616d65')
+    last_name = needed_values_dict.get('4c617374204e616d65')
+    year = needed_values_dict.get('Year')
+    make = needed_values_dict.get('4d616b6520616e64204d6f64656c')
+    length = needed_values_dict.get('Length')
+    msg_subject = f"{last_name}, {first_name} | {year} {make} {length} | New Quote Submission"
+    return msg_subject
 
 def Get_Add_Notes():
-	additional_notes = additional_email_body_notes.get()
-	return additional_notes
+    additional_notes = additional_email_body_notes.get()
+    return additional_notes
 
 def Get_CC_Addresses():
-	config = read_config()
-        cc_addresses = [cc_address_1_user_input.get(), cc_address_2_user_input.get()]
-	if config.get('CarbonCopy Settings', 'settings_merge_cc_addresses')=='0':
-	    try:
-		cc_addresses.append(config.get('CarbonCopy Settings', ''), config.get('CarbonCopy Settings', ''))
-	    except
-	        print('The cc append didnt work, trying the other way...')
-		cc_addresses.append(config.get('CarbonCopy Settings', '')
-                cc_addresses.append(config.get('CarbonCopy Settings', '')
-	        print('Done.')
-        #cc_total = f"{insertdefaultcc1}; {cc_1}; {insertdefaultcc2}; {cc_2}"
+    config = read_config()
+    cc_addresses = [cc_address_1_user_input.get(), cc_address_2_user_input.get()]
+    if config.get('CarbonCopy Settings', 'settings_merge_cc_addresses')=='0':
+        try:
+            cc_addresses.append(config.get('CarbonCopy Settings', ''), config.get('CarbonCopy Settings', ''))
+        except:
+            print('The cc append didnt work, trying the other way...')
+            try:
+                cc_addresses.append(config.get('CarbonCopy Settings', ''))
+                cc_addresses.append(config.get('CarbonCopy Settings', ''))
+                print('Done.')
+            except:
+                print('Not successful in saving CC addresses')
         return cc_addresses
 
 def path_to_additional_attachments(event):
@@ -96,11 +138,7 @@ def passing():
 	pass
 
 				    
-#Main Functions
-
-def btnClickFunction():
-	#TO POPULATE
-	
+#MAIN FUNCTIONS-------|
 def sameCarrierSubmission():
     config = read_config()
     dict0 = [seawave_check.get(), primetime_check.get(), newhampshire_check.get()]
@@ -110,28 +148,24 @@ def sameCarrierSubmission():
         body = config.get('Combo email', 'sw_and_pt_body')
         salutation = config.get('SW email', 'salutation')
         your_name = config.get('General settings', 'your_name')
-        sendEmail(address, greeting, body, salutation, your_name)
     elif dict0[0]==1 and dict0[1]==0 and dict0[2]==1:
         address = config.get('SW email', 'address')
         greeting = config.get('SW email', 'greeting')
         body = config.get('Combo email', 'sw_and_nh_body')
         salutation = config.get('SW email', 'salutation')
         your_name = config.get('General settings', 'your_name')
-        sendEmail(address, greeting, body, salutation, your_name)
     elif dict0[0]==1 and dict0[1]==1 and dict0[2]==1:
         address = config.get('SW email', 'address')
         greeting = config.get('SW email', 'greeting')
         body = config.get('Combo email', 'pt_and_nh_and_sw_body')
         salutation = config.get('SW email', 'salutation')
         your_name = config.get('General settings', 'your_name')
-        sendEmail(address, greeting, body, salutation, your_name)
     elif dict0[1]==1 and dict0[2]==1:
         address = config.get('SW email', 'address')
         greeting = config.get('SW email', 'greeting')
         body = config.get('Combo email', 'pt_and_nh_body')
         salutation = config.get('SW email', 'salutation')
         your_name = config.get('General settings', 'your_name')
-        sendEmail(address, greeting, body, salutation, your_name)
     elif dict0[0]==1 or dict0[1]==1 or dict0[2]==1:
         if dict0[0]==1:
             address = config.get('SW email', 'address')
@@ -139,21 +173,18 @@ def sameCarrierSubmission():
             body = config.get('SW email', 'body')
             salutation = config.get('SW email', 'salutation')
             your_name = config.get('General settings', 'your_name')
-            sendEmail(address, greeting, body, salutation, your_name)
         elif dict0[1]==1:
             address = config.get('PT email', 'address')
             greeting = config.get('PT email', 'greeting')
             body = config.get('PT email', 'body')
             salutation = config.get('PT email', 'salutation')
             your_name = config.get('General settings', 'your_name')
-            sendEmail(address, greeting, body, salutation, your_name)
         else:
             address = config.get('NH email', 'address')
             greeting = config.get('NH email', 'greeting')
             body = config.get('NH email', 'body')
             salutation = config.get('NH email', 'salutation')
             your_name = config.get('General settings', 'your_name')
-            sendEmail(address, greeting, body, salutation, your_name)
     else:
         pass
     for section in config:
@@ -161,14 +192,14 @@ def sameCarrierSubmission():
             pass
         else:
             try:
-	        address = config.get(section, 'address')
+                address = config.get(section, 'address')
                 greeting = config.get(section, 'name')
                 body = config.get(section, 'name')
                 salutation = config.get(section, 'salutation')
                 your_name = config.get('General Settings', 'your name')
-                sendEmail(address, greeting, body, salutation, your_name)
-	    except:
-		pass
+            except:
+                pass
+    sendEmail(address, greeting, body, salutation, your_name)
 
 def sendEmail(address, greeting, body, salutation, your_name):
     import win32com.client as win32
@@ -233,6 +264,10 @@ tabControl.add(settings, text='Settings')
 tabControl.pack(expand=1, fill="both")
 #tkinter modules by tab
 #main
+
+frame3 = Frame(main)
+frame3.grid(row=0, rowspan=10, column=3)
+
 seawave_check = tk.IntVar()
 primetime_check = tk.IntVar()
 newhampshire_check = tk.IntVar()
@@ -260,7 +295,7 @@ Label(main, text='Additional e-mail Notes', bg='#5F9EA0', font=('helvetica', 12,
 Label(main, text='CC-Address 1:', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=4, column=1, pady=10)
 Label(main, text='CC-Address 2:', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=7, column=1, pady=15) 
 Label(main, text='Additional Notes/CC', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=1, pady=25)
-Label(main, text='Markets to Submit to:', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=3, pady=15)
+Label(frame3, text='Markets to Submit to:', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=3, pady=15)
 
 seawave = Checkbutton(frame3, text='Seawave Insurance', variable=seawave_check, bg='#7FFFD4', font=('helvetica', 12, 'normal'))
 seawave.grid(row=1, column=3)
@@ -290,17 +325,17 @@ cc_address_1_user_input.grid(row=5, column=1)
 cc_address_2_user_input = Entry(main)
 cc_address_2_user_input.grid(row=8, column=1)
 
-Button(main, text='Submit and sent to markets!', bg='#7FFFD4', font=('helvetica', 12, 'normal'), command=btnClickFunction).grid(row=11, column=3, pady=10)
+Button(frame3, text='Submit and sent to markets!', bg='#7FFFD4', font=('helvetica', 12, 'normal'), command=sameCarrierSubmission).grid(row=11, column=3, pady=10)
 
 #settings
-cc_def_1 = Radiobutton(settings, text="Add user input to the below.", variable=settings_merge_cc_addresses, value='0')
-cc_def_1.grid(row=3, column=0)
-cc_def_2 = Radiobutton(settings, text="Replace the below with user input.", variable=settings_merge_cc_addresses, value='1')
-cc_def_2.grid(row=4, column=0)
-cc_def_address_1 = Entry(settings)
-cc_def_address_1.grid(row=6, column=0)
-cc_def_address_2 = Entry(settings)
-cc_def_address_2.grid(row=8, column=0)
+cc_merge_0 = Radiobutton(settings, text="Add user input to the below.", variable=settings_merge_cc_addresses, value='0')
+cc_merge_0.grid(row=3, column=0)
+cc_merge_1 = Radiobutton(settings, text="Replace the below with user input.", variable=settings_merge_cc_addresses, value='1')
+cc_merge_1.grid(row=4, column=0)
+def_cc_address_1 = Entry(settings)
+def_cc_address_1.grid(row=6, column=0)
+def_cc_address_2 = Entry(settings)
+def_cc_address_2.grid(row=8, column=0)
 
 Label(settings, text='Settings Page', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=0, columnspan=2, pady=10)
 Label(settings, text='General Settings', bg='#5F9EA0', font=('helvetica', 14, 'normal')).grid(row=1, column=0, pady=10)
@@ -308,11 +343,11 @@ Label(settings, text='Preference on merging CC-addresses:', bg='#5F9EA0', font=(
 Label(settings, text='Insert address to always CC', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=5, column=0, pady=10)
 Label(settings, text='Insert another address to CC', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=7, column=0, pady=10)
 
-Button(settings, text='Save Settings!', bg='#7FFFD4', font=('helvetica', 12, 'normal'), command=btnSave_Settings).grid(row=11, column=3, pady=10)
+Button(settings, text='Save Settings!', bg='#7FFFD4', font=('helvetica', 12, 'normal'), command=btnSaveMainSettings).grid(row=11, column=3, pady=10)
 
 #email_template
-Label(settings, text = 'Email Template Adjustment', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=0, pady=10)
-Label(settings, text = 'Select a specific market, or one of the combo options, to change the template for the email message.', bg='#5F9EA0', font=('helvetica', 14, 'normal')).grid(row=1, column=0, columnspan=3 pady=10)
+Label(template_settings, text = 'Email Template Adjustment', bg='#5F9EA0', font=('helvetica', 16, 'normal')).grid(row=0, column=0, pady=10)
+Label(template_settings, text = 'Select a specific market, or one of the combo options, to change the template for the email message.', bg='#5F9EA0', font=('helvetica', 14, 'normal')).grid(row=1, column=0, columnspan=3, pady=10)
 
 options = [
 	"Select Carrier"
@@ -326,37 +361,33 @@ options = [
 	"Century"
 	"Intact"
 	"Travelers"
-	"Combo SW and TP"
+	"Combo SW and PT"
 	"Combo SW and NH"
 	"Combo SW, PT and NH"
 	"Combo PT and NH"
 ]
 dropdown_email_template = StringVar()
 dropdown_email_template.set('Select Carrier')
-drop = OptionMenu(settings, dropdown_email_template, *options)
+drop = OptionMenu(template_settings, dropdown_email_template, *options)
 drop.grid(row=2, column=1)
 
-Label(settings, text = 'Submission Address: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=3, column=0, pady=10)
-Label(settings, text = 'Greeting: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=4, column=0, pady=10)
-Label(settings, text = 'Body of the email: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=5, column=0, pady=10)
-Label(settings, text = 'Salutation: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=6, column=0, pady=10)
-Label(settings, text = 'Your name: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=7, column=0, pady=10)
-carrier_ = Entry(main)
-cc_def_1_entry.grid(row=5, column=1)
-cc_def_2_entry = Entry(main)
-cc_def_2_entry.grid(row=8, column=1)
+Label(template_settings, text = 'Submission Address: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=3, column=0, pady=10)
+Label(template_settings, text = 'Greeting: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=4, column=0, pady=10)
+Label(template_settings, text = 'Body of the email: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=5, column=0, pady=10)
+Label(template_settings, text = 'Salutation: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=8, column=0, pady=10)
+Label(template_settings, text = 'Your name: ', bg='#5F9EA0', font=('helvetica', 12, 'normal')).grid(row=9, column=0, pady=10)
+carrier_address = Entry(template_settings)
+carrier_address.grid(row=3, column=1)
+carrier_greeting = Entry(template_settings)
+carrier_greeting.grid(row=4, column=1, columnspan=2)
+carrier_body = Entry(template_settings)
+carrier_body.grid(row=5, column=1, columnspan=2)
+carrier_salutation = Entry(template_settings)
+carrier_salutation.grid(row=8, column=1, columnspan=2)
+your_name = Entry(template_settings)
+your_name.grid(row=9, column=1)
 
-button = Button(settings, text = "Save!", command = btn_save_carrier_details).grid(row=4, column=1)
-#these are the declarations of the variables associated with the checkboxes
-
-#frame1 = Frame(main, bg='red', padx=2, pady=2)
-#frame1.place(x=0, y=0)
-#frame2 = Frame(main, bg='green', padx=2, pady=2)
-#frame2.place(x=265, y=0)
-#frame3 = Frame(main, bg='green', padx=2, pady=2)
-#frame3.place(x=475, y=0)
-
-# This is the section of code which creates the checkboxes
+button = Button(settings, text = "Save!", command = btn_save_carrier_template).grid(row=4, column=1)
 
 #SETTING THE WINDOW TO CENTER
 # get the screen dimension
