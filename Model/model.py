c53f94870d1e1cc071ae388dd76a629a012dd1d7
Sam <7569798+Samlant@users.noreply.github.com>
2023-02-11 00:34:28 -0500

    Update model.py
    Removed unused methods from model class and finished most model existing functions

--- Model/model.py
+++ Model/model.py
@@ -13,35 +13,14 @@ class Model:
 
     self.positive_submission = None
 
-    # FNs used to SAVE stuff
-    # def handle_save_contents(self, section_name: str, save_contents: dict) -> bool: #NEED TO FINISH !!!
-    #     """ This is a generic function to save all three save buttons' data to the appropriate config section. It also ensures the section exists.
-    #     """
-    #     config = self.open_config()
-    #     self.validate_section()
-
-    #     for key, value in save_contents:
-    #         config.
-    
-
-# THIS SECTION HAS GOOD, FINAL CODE UNTIL REVIEW ON LATE SATURDAY...
-
     def handle_redundancies(self, carrier_checkboxes: dict) -> dict:
         if self.redundancy_check(carrier_checkboxes):
             section_name = str(self.fix_redundancies(carrier_checkboxes))
             new_single_submission = dict(section_name, 'submit') #hardcode value
             return new_single_submission
         else:
-            pass
-        # we need to complete the else statement,  to automatically connect to the next step of the process,  which at time of writing isn't figured out yet.
-                                                     
+            return carrier_checkboxes
         
-
-    def 
-
-
-
-
     def redundancy_check(self, carrier_checkboxes: dict) -> bool: #Recheck
         """ Checks the redundancy list that the view provides for duplicates."""
         check_list = list()
@@ -53,7 +32,6 @@ class Model:
             return False
         else:
             raise ValueError()
-
     
     def fix_redundancies(self, carrier_checkboxes: dict) -> str: #GOOD
         """ Receives a dict of name:boolean where it finds the two---or three---key:value pairs & then assigns the correct config section name.  This allows the program to access the proper data for the envelope to be sent.
@@ -75,119 +53,29 @@ class Model:
         else:
             raise ValueError
 
-
-# END OF GOOD, FINAL CODE..
-
-
-    def set_initial_view_values(self): #NEED TO MOVE TO PRESENTER
-        ''' Set the entries and textboxes on the 
-        template page to disabled since the default 
-        drop-down isn't a valid choice.
-        '''
-        #getPlaceHolder(list)
-        #insertPlaceholder(list)
-        # MOVE THIS IMMEDIATE BELOW TO THE VIEW, create fN to insert/configure
-        self.view.carrier_address.insert(0, carrier_address_placeholder)
-        self.view.carrier_address.configure(state='disabled')
-        self.view.carrier_greeting.insert(0, carrier_greeting_placeholder)
-        self.view.carrier_greeting.configure(state='disabled')
-        self.view.carrier_body.insert(1.0, carrier_body_placeholder)
-        self.view.carrier_body.configure(state='disabled', wrap='word')
-        self.view.carrier_salutation.insert(0, carrier_salutation_placeholder)
-        self.view.carrier_salutation.configure(state='disabled')
-
-    def savePath(self, raw_path, is_quoteform: bool):#GOOD
-        path = self.__validatePath(raw_path)
+    def save_path(self, raw_path, is_quoteform: bool):#GOOD
+        path = self.clean_path(raw_path)
         if is_quoteform == True:
             self.quoteform_path = path
         elif is_quoteform == False:
             self.extra_attachments.append(path)
         else:
             print('Raising type error: is_quoted parameter is either empty or wrong type. It needs to be boolean.')
-    
-    def save_extra_notes(self, notes: str) -> None: #GOOD
-        self.extra_notes = notes
 
-    def getAllAttachments(self) -> list:
-         self.attachments.append(self.quoteform_path)
+    def get_all_attachments(self) -> list:
+         self.attachments = []
+self.attachments.append(self.quoteform_path)
          self.attachments.append(self.extra_attachments)
          return self.attachments
 
-    def __validatePath(self, path) -> str:
+    def clean_path(self, path) -> str:
         """ Checks if brackets exists and removes if so."""
         if '{' in path.data:
             path = path.data.translate({ord(c): None for c in '{}'})
         else:
             return None
         return path
-    
-    def check_list(self, num: int) -> bool:
-        if num > 1:
-            return True
-        elif num <= 1:
-            return False
-        else:
-            print('This model function failed to count.')
-            return None
         
-    def check_if_combo(self, carrier_checkboxes: dict) -> bool: #GOOD
-        """ This checks if a combo submission is required."""
-        #Presenter.check_if_combo is a possibility, not needed ATM.
-        list = [carrier_checkboxes.get('sw'), carrier_checkboxes.get('pt'), carrier_checkboxes.get('nh')]
-        if list.count('submit') >= 2:
-            return True
-        else:
-            return False
-        
-    def check_if_duplicates_exist(self, possible_duplicates: list, input: dict):#CHECK AND FIX THIS FUNCTION.. THE WHOLE PROCESS TO CHECK FOR DUPLICATES;  REVISE & RESTRUCTURE
-        num = self.count_list(input)
-        check_bool = self.check_list(num)
-        if check_bool:
-            list = self.add_submits_to_list(input)
-            section_name = self.get_correct_combination(list)
-            var1 = self.get_config_value(section_name, 'address')
-            var2 = self.get_config_value(section_name, 'body')
-            self.assign_correct_combination(list)
-        elif not check_bool:
-            pass
-        else:
-            print('This model function did not perform as expected.')
-            return None
-
-    def get_correct_combination(self, input:list) -> str:
-        """ This gets the correct section name of the config file that we want to access by inserting both markets' names from the list into a pre-structured string.
-         """
-        if num == 2:
-            output_str = f"Combination: {input[0]} and {input[1]}"
-            return output_str
-        elif num == 3:
-            output_str = f"Combination: {input[0]} and {input[1]} and {input[2]}"
-            return output_str
-        else:
-            print('Not offering four combo markets- check this model function for errors in unexpected input & accomodate for more if needed.')
-            return None
-
-    def count_list(self, input:dict) -> int:
-        return input.items().count('Submit')
-
-    def add_submits_to_list(self, input: dict) -> list:
-        submitting_list = list()
-        for carrier, result in input:
-            if result == 'submit':
-                submitting_list.append(carrier)
-            elif result == 'skip':
-                pass
-            else:
-                print('this model function did not receive input as expected.')
-        return submitting_list
-
-    def saveCC(self, input):
-        #check if ignore defaults
-        pass
-
-
-
-
 class ConfigWorker:
     """ This class handles all interactions between the python and config file. It utilizes open_config() as a helper to acces config, discerns the path of flowing information & then performs those queries on the config file.
     """
@@ -218,4 +106,14 @@ class ConfigWorker:
         """ This returns the section keys:values in a dict"""
         config = self.open_config()
         return config.get_section(section_name, 'Error section')
+        
+    # def handle_save_contents(self, section_name: str, save_contents: dict) -> bool: #NEED TO FINISH !!!
+    #     """ This is a generic function to save both Save buttons' data to the appropriate config section. It also ensures the section exists.
+    #     """
+    #     config = self.open_config()
+    #     if self.validate_section(section_name):
+                
+
+    #     for key, value in save_contents:
+    #         config.update(section_name, key, value)
     # End of CONFIG FILE operations
