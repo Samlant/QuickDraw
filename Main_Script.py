a6f78521c1be3fc2158e974d3428045966aa276c
Sam <7569798+Samlant@users.noreply.github.com>
2023-02-28 18:32:06 -0500

    temop

--- Main_Script.py
+++ Main_Script.py
@@ -2,12 +2,14 @@ from Model.model import Model, ConfigWorker
 from View.view import TkView
 from Presenter.presenter import Presenter
 
+
 def main() -> None:
     model = Model()
     view = TkView()
     configworker = ConfigWorker()
     presenter = Presenter(model, view, configworker)
     presenter.start_program()
+
 
 if __name__ == "__main__":
     main()
