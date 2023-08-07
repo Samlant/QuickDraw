030498c09437a3c527b8fe8d3b5e95ea16008067
Sam L <7569798+Samlant@users.noreply.github.com>
2023-08-06 22:37:55 -0400

    Adds foundational documentation of the program.
    Adds good starting base of documentation for the end-user

---
+++ docs/docs/install.md
@@ -0,0 +1,13 @@
+# Installation Details
+
+After running setup.exe, the program installs itself to your user's folder
+
+    C:\Users\your_user_name\AppData\Local\Worktools
+
+as well as make a startmenu shortcut, desktop shortcut and adds itself to the boot-up sequence.
+
+You may access the program from
+
+- the system tray icon in the lower-right corner
+- the StartMenu in the lower-left corner (or Windows key)
+- Desktop icon
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,22 @@
-import os
-import time
-import sys
-
-import pystray
 import time
 
-from view.test_sys_icon import TrayIcon
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
 
 
-tray_icon = TrayIcon()
-tray_icon.create_icon()
-while tray_icon.active == True:
-    
+class WebClient:
+    def __init__(self) -> None:
+        self.options = Options()
+        self.options.page_load_strategy = "normal"
+
+    def engage_browser(self, uri: str):
+        driver = webdriver.Edge(options=options)
+        driver.get(uri)
+        print(driver.title)
+        while driver.title == "Google":
+            print("Still waiting for a new page")
+            time.sleep(2)
+        print(driver.current_url)
+        uri = driver.current_url
+        driver.quit()
+        return uri
