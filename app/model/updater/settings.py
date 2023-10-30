b1282d03b26df8f32a3c20ad488dd20c37d12add
Sam L <7569798+Samlant@users.noreply.github.com>
2023-10-29 20:46:25 -0400

    Update settings.py

--- app/model/updater/settings.py
+++ app/model/updater/settings.py
@@ -11,7 +11,7 @@ APP_VERSION = "3.0.0"
 
 # Current module dir (when frozen this equals sys._MEIPASS)
 # https://pyinstaller.org/en/stable/runtime-information.html#using-file
-MODULE_DIR = Path(__file__).resolve().parent
+MODULE_DIR = Path(__file__).resolve().parents[2]
 
 # Are we running in a PyInstaller bundle?
 # https://pyinstaller.org/en/stable/runtime-information.html
@@ -20,7 +20,7 @@ print("Are we frozen?")
 print(FROZEN)
 print(f"The MODULE_DIR is: {MODULE_DIR}")
 # For development
-DEV_DIR = MODULE_DIR.parent.parent.parent / "repo"
+DEV_DIR = MODULE_DIR.parent / "repo"
 print(f"The DEV_DIR is: {DEV_DIR}")
 # App directories
 
@@ -37,14 +37,16 @@ METADATA_DIR = UPDATE_CACHE_DIR / "metadata"
 TARGET_DIR = UPDATE_CACHE_DIR / "targets"
 
 # Update-server urls
-METADATA_BASE_URL = ""
-TARGET_BASE_URL = ""
+METADATA_BASE_URL = (
+    "https://raw.githubusercontent.com/Samlant/updater/blob/main/metadata"
+)
+TARGET_BASE_URL = "https://raw.githubusercontent.com/Samlant/updater/blob/main/targets"
 
 # Location of trusted root metadata file
-TRUSTED_ROOT_SRC = MODULE_DIR / "_internal" / "root.json"
+TRUSTED_ROOT_SRC = MODULE_DIR / "root.json"
 if not FROZEN:
     # for development, get the root metadata directly from local repo
-    sys.path.insert(0, str(MODULE_DIR.parent.parent.parent))
+    sys.path.insert(0, str(MODULE_DIR.parent))
     from repo_settings import REPO_DIR
 
     TRUSTED_ROOT_SRC = REPO_DIR / "metadata" / "root.json"
