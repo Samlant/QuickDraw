3518bff44ef3d27177b2c1c8d601af838e29dc80
Sam <7569798+Samlant@users.noreply.github.com>
2024-01-14 10:06:32 -0500

    Update quoteform_registrations.py

--- app/model/quoteform_registrations.py
+++ app/model/quoteform_registrations.py
@@ -1,30 +1,14 @@
-def assign_registration_name(config) -> str:
-    counter = 1
-    name = "Form_"
-    test_name = f"{name}{counter}"
-    while config._validate_section(test_name):
-        counter += 1
-        test_name = f"{name}{counter}"
-    return test_name
-
-
-# def check_if_duplicate(quoteform, config):
-#     conf = config._open_config()
-#     quoteform_names = [y for y in conf.sections() if "Form_" in y]
-
-#     quoteforms: list[dict[str, str]] = []
-
-#     for saved_quoteform in quoteform_names:
-#         new_dict = {}
-#         section = config.get_section(saved_quoteform)
-#         options = section.items()
-#         for x, y in options:
-#             new_dict[x] = y.value
-#         quoteforms.append(new_dict)
-
-#     for saved_quoteform in quoteforms:
-#         if quoteform == saved_quoteform:
-#             return True
-#         else:
-#             continue
-#     return False
+def remove_all_sections(config, names: list[str]):
+    conf = config._open_file()
+    for name in names:
+        conf.remove_section(name)
+        
+def standardize_name(name: str) -> str:
+    return f"Form_{name}"
+    
+def validate_name(existing_names: list[str], names: str):
+    if name in existing_names:
+        return False
+    else:
+        return True
+        
