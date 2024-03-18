3cf1c775dfcd3b8fd4990ae3f644772cf718c8b0
Sam L <7569798+Samlant@users.noreply.github.com>
2024-03-18 08:30:02 -0400

    almost brought app to ground zero.  Need work on GUI

---
+++ quick.py
@@ -0,0 +1,6 @@
+uninstalled = """flickr-api exceptiongroup"""
+
+print("Need to install in o.\rder to have same env as work laptop!")
+print(uninstalled.replace(" ", "\n"))
+
+current_in_venv = """altgraph attrs bsdiff4 certifi cffi charset-normalizer ConfigUpdater cryptography datefinder et-xmlfile fillpdf h11 idna msal outcome packaging parse pdf2image pdfrw2 pefile pillow pip-chill pip-review pycparser pyinstaller pyinstaller-hooks-contrib PyJWT PyMuPDF PyMuPDFb PyNaCl pyparser pypiwin32 PySocks pystray python-dateutil python-dotenv pytz pywin32 pywin32-ctypes regex requests securesystemslib selenium setuptools six sniffio sortedcontainers tkinterdnd2 trio trio-websocket tuf tufup @ git+https://github.com/dennisvang/tufup.git@735231c7dffce1aa46200725a2036087e5e6b458 urllib3 win10toast wsproto"""
